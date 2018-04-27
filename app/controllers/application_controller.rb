b96021ef6645031a480d23156d856aaf4422edd1
fegomezmir <fagomez1@miuandes.cl>
2018-04-26 22:52:09 -0300

    ahora si, tenemos bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  Rails.application.config.session_store :cookie_store, key: "current_user_id"
+
+
+
+def current_user
+  @_current_user ||= session[:current_user_id] &&
+      User.find_by(id: session[:current_user_id])
+end
+
+  def is_user_logged_in?
+    logged_in = false
+    #complete this method
+    #flash[:notice] = session[:current_user_id]
+    flash[:notice] = current_user
+
+    if current_user
+      logged_in = true
+    else
+      logged_in = false
+    end
+    if logged_in  == true
+      true
+    else
+      #redirect_to root_path
+    end
+
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,31 @@
 class SessionsController < ApplicationController
   def new
   end
+
+
+  def create
+    if user = User.authenticate(params[:email], params[:password])
+      # Save the user ID in the session so it can be used in
+      # subsequent requests
+      session[:current_user_id] = user.id
+      redirect_to root_url
+    end
+
+    if is_user_logged_in? == true
+      #redirect_to users_url
+
+    else
+      #redirect_to root_path
+
+    end
+
+  end
+
+
+
+end
+
+
+def session_params
+  params.require(:sessions).permit(:email, :password)
 end
