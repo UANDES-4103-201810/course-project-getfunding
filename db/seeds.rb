90f2fa51327babc3a27793191b0328c14b6b32a0
Web <cerenchun@miuandes.cl>
2018-05-01 23:24:42 -0300

    intento seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#projects = projects.create([{User},{name =},{description},{goal},{money_colected},{expiration_date},{state},{start_date}])
+#project.destroy_all
+
+#project.create([{
+ #                   User:1
+  #                 name:""
+ #                   description:""
+ #                   goal:2
+ #                   money_colected:
+#                   expiration_date:
+#                   state:
+#                   start_date:
+#               },
+#               {
+#                   User:
+#                       name:
+#                       description:
+#                       goal:
+#                       money_colected:
+#                       expiration_date:
+#                       state:
+#                       start_date:
+#               },
+#               {
+#                   User:
+#                       name:
+#                       description:
+#                       goal:
+#                       money_colected:
+#                       expiration_date:
+#                       state:
+#                       start_date:
+#               }])
+#
+#create_table :projects do |t|
+# t.references :User, foreign_key: true
+# t.string :name
+# t.text :description
+# t.integer :goal
+# t.integer :money_colected
+# t.date :expiration_date
+## t.string :state
+# t.date :start_date
+#
+# t.timestamps
